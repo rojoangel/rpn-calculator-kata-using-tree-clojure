cebd6e42902836cc8589e8be0b3fb7210fd5e904
Angel Rojo Marijuan <arojomarijuan@magento.com>
2017-02-07 21:29:29 +0100

    refactor: extrat "store-number" private function

--- src/rpn/core.clj
+++ src/rpn/core.clj
@@ -1,9 +1,11 @@
 (ns rpn.core
   (:require [clojure.string :as str]))
 
+(defn- store-number [n tree]
+  (cons (read-string n) tree))
+
 (defn calculate [operation]
   (let [tokens (str/split (str/trim operation) #"\s+")]
     (str/join
     " "
-    (reduce
-      #(cons (read-string %1) %2) tokens))))
+    (reduce store-number tokens))))
