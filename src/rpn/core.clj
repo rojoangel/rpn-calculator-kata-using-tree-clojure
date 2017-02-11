a40779a48d6cf6f10ca8306b868efc81b0b12fa2
Angel Rojo Marijuan <arojomarijuan@magento.com>
2017-02-11 13:41:45 +0100

    implement walk function

--- src/rpn/core.clj
+++ src/rpn/core.clj
@@ -5,22 +5,21 @@
   (reverse (cons n (reverse tree))))
 
 (defn- store-operation [tree op]
-  (let [fn (resolve op)]
-    (reverse
+  (reverse
     (cons
-      (cons fn (take-last 2 tree))
+      (cons op (take-last 2 tree))
       (reverse
-        (drop-last 2 tree))))))
+        (drop-last 2 tree)))))
 
-(defn- store [tree symbol ]
+(defn- store [tree symbol]
   (if (number? symbol)
     (store-number tree symbol)
     (store-operation tree symbol)))
 
-(defn- walk [tree]
-  (if (seq? (first tree))  ;; then, the first item is an operation
-     (cons (eval (first tree)) (rest tree))
-     tree))
+(defn- walk [branch]
+  (if (seq? branch)
+    (eval (list (resolve (first branch)) (nth branch 1) (nth branch 2)))
+    branch))
 
 (defn- format [tree]
   (str/join " " tree))
@@ -28,4 +27,4 @@
 (defn calculate [operation]
   (let [tokens (str/split (str/trim operation) #"\s+")
         symbols (map read-string tokens)]
-    (format (walk (reduce store nil symbols)))))
+    (format (map walk (reduce store nil symbols)))))
